356b9a6fb57db051d10795eaaeb7fe0bb8c0e101
Moli <143495156+Moli310@users.noreply.github.com>
2025-11-08 16:04:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ def find_asset(filename, folder="assets"):
     return None
 
 # ---------- Helper: Set Background ----------
-def set_background(image_path):
-    p = Path(image_path)
+def set_background(assets/Hogwarts.jpg):
+    p = Path(assets/Hogwarts.jpg)
     if not p.exists():
         found = find_asset(p.name, p.parent)
         if found:
